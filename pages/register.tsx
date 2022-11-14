d40c875c5ceb656e3a4c9cba6b2ec495205fb2d1
FyodorKudritsky <fedosdekudrille@mail.ru>
2022-11-14 13:03:59 +0300

    dynamic color change in progress bar

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,13 +1,30 @@
-import {ChakraProvider} from "@chakra-ui/react";
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
 import React from "react";
+import {useState} from "react";
 import LayoutCard from "../components/LayoutCard";
 import RegisterCard from "../components/RegisterCard";
 
 const Register = () => {
+  const [progressColor, changeProgressColor] = useState<string>("red");
+  function changeColor(color: string) {
+    changeProgressColor(color);
+  }
+  const theme = extendTheme({
+    colors: {
+      progressColor: {
+        500: progressColor,
+      },
+    },
+  });
   return (
     <LayoutCard>
-      <ChakraProvider>
-        <RegisterCard />
+      <ChakraProvider theme={theme}>
+        <RegisterCard
+          changeProgressColor={(color: string) => {
+            changeProgressColor(color);
+            console.log(color);
+          }}
+        />
       </ChakraProvider>
     </LayoutCard>
   );
