fd487152e6c2d0b4a255bf36cd20d042bfe69c36
Даник <lobunetsdaniil@yandex.ru>
2022-12-05 16:44:58 +0300

    firestorebd rework

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -7,35 +7,98 @@ import {
   Input,
   Icon,
   Stack,
+  Tooltip,
+  Box,
 } from "@chakra-ui/react";
 import React, {useState, useContext, useEffect, memo} from "react";
 import {BiSearchAlt2} from "react-icons/bi";
 import {CiSettings} from "react-icons/ci";
+import {MdAdd} from "react-icons/md";
 import {mainStyles} from "./LayoutCard";
 import {db} from "../firebaseconfig";
-import {collection, query, where, getDocs} from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import {AuthContext} from "../context/AuthContext";
 
 type ChatItemProps = {
   searchedAvatar?: string;
   searchedName?: string;
+  searchedUser?: any;
+  options?: boolean;
 };
 
 export const ChatItem = memo(
-  ({searchedAvatar = "", searchedName = "User"}: ChatItemProps) => {
+  ({
+    searchedAvatar = "",
+    searchedName = "User",
+    searchedUser,
+    options,
+  }: ChatItemProps) => {
+    const currentUser: any = useContext(AuthContext);
+    const setToChatList = async () => {
+      const combinedUid: any =
+        currentUser.uid.slice(0, 5) + "." + searchedUser?.userID!.slice(0, 5);
+      const docRef: any = doc(db, "chats", combinedUid);
+      const existed: any = await getDoc(docRef);
+
+      if (true) {
+        console.log("yes");
+        await setDoc(doc(db, "chats", combinedUid), {messages: []});
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedUid + ".userInfo"]: {
+            uid: searchedUser.userID,
+            displayName: searchedUser.displayName,
+            photoURL: searchedUser.photoURL,
+          },
+          [combinedUid + ".date"]: serverTimestamp(),
+        });
+      }
+    };
+
     return (
       <Flex
         align="center"
+        justify="space-between"
         p="2"
         _hover={{bg: mainStyles.chatListItemHover, cursor: "pointer"}}
         w="100%"
         px={4}
         py={2}
       >
-        <Avatar src={searchedAvatar} />
-        <Text ms={3} color="white">
-          {searchedName}
-        </Text>
+        <Flex align="center">
+          <Avatar src={searchedAvatar} />
+          <Text ms={3} color="white">
+            {searchedName}
+          </Text>
+        </Flex>
+        <Flex align="center">
+          {options && (
+            <Tooltip label="Add user to chat list">
+              <Flex align="center">
+                <Icon
+                  as={MdAdd}
+                  color={mainStyles.mainIconColor}
+                  boxSize="25px"
+                  transition="color 200ms linear"
+                  onClick={setToChatList}
+                  _hover={{
+                    color: mainStyles.mainItemColor,
+                    cursor: "pointer",
+                  }}
+                />
+              </Flex>
+            </Tooltip>
+          )}
+        </Flex>
       </Flex>
     );
   }
@@ -59,9 +122,9 @@ export const ChatSearch = ({
   const handleSearch = async () => {
     const queryDB = query(
       collection(db, "users"),
-      // where("displayNameLC", "==", username.toLocaleLowerCase()),
+      where("displayNameLC", "==", username.toLocaleLowerCase())
       // where("userID", "!=", currentUser.uid)
-      where("displayNameLC", ">=", username.toLocaleLowerCase())
+      // where("displayNameLC", ">=", username.toLocaleLowerCase())
       // where("displayNameLC", "<=", username.toLocaleLowerCase() + "~")
     );
     try {
@@ -132,6 +195,8 @@ const Render = ({searchedUsers, username}: any) => {
       <ChatItem
         searchedAvatar={user.photoURL}
         searchedName={user.displayName}
+        searchedUser={user}
+        options
         key={Math.random()}
       />
     );
